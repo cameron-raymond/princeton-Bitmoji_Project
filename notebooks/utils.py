2f326b840314abc4f449deb71b788901dd56d8c7
Cameron Raymond <cameron.raymond@hey.com>
2021-06-15 14:52:17 +0100

    pink obj parser

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -6,3 +6,10 @@ green = {
     'high_hsv':  np.array([80,250,250]),
     'category': 'poster'
 }
+
+pink = {
+    'rgb': np.array([255,64,255]),
+    'low_hsv': np.array([100,150,200]),
+    'high_hsv':  np.array([200,225,250]),
+    'category': 'device'
+}
