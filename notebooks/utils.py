06970f40834bc4f616919f522c810dfec95b4e9c
Cameron Raymond <cameron.raymond@hey.com>
2021-06-20 12:57:04 +0100

    bitmoji working with "official" colours

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,17 +1,36 @@
 import numpy as np
 
-green = {
-    'rgb': np.array([120,250,250]),
-    'low_hsv': np.array([50,100,180]),
-    'high_hsv':  np.array([80,250,250]),
-    'category': 'poster'
+lime = {
+    'rgb': np.array([0,255,0]),
+    'hex': '#00ff00',
+    'low_hsv': np.array([50, 230,230]),
+    'high_hsv': np.array([70, 260,260]),
+    'category': '01_lime'
+}
+
+orange = {
+    'rgb': np.array([255,165,0]),
+    'hex': '#ffa500',
+    'low_hsv': np.array([15,210, 230]),
+    'high_hsv': np.array([30,300,300]),
+    'category': '02_orange'
+}
+
+blue = {
+    'rgb': np.array([0,0,255]),
+    'hex': '#0000ff',
+    'low_hsv': np.array([80, 240,190]),
+    'high_hsv': np.array([130, 260,250]),
+    'category': '03_blue'
 }
 
 pink = {
-    'rgb': np.array([255,64,255]),
-    'low_hsv': np.array([140,150,200]),
-    'high_hsv':  np.array([200,225,250]),
-    'category': 'device'
+    'rgb': np.array([255,20,147]),
+    'hex': '#ff1493',
+    'low_hsv': np.array([160,200, 220]),
+    'high_hsv': np.array([170,260,260]),
+    'category': '04_pink'
 }
 
-categories = [green,pink]
+
+categories = [lime,orange,blue,pink]
